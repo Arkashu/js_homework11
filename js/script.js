bf6d370be8f3fd5178f49e9763131f02731cf521
Arkashu <kopeikaarkashu@gmail.com>
2022-07-13 18:49:49 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,91 @@
+'use strict'
+//Task1
+// let a = 0;
+// a === 0 ? console.log('Верно') : console.log('Не верно');
+
+// Task2
+// let a = 0;
+// a > 0 ? console.log('Верно') : console.log('Не верно');
+
+// Task3
+// let a = 0;
+// a < 0 ? console.log('Верно') : console.log('Не верно');
+
+// Task4
+// let a = 0;
+// a >= 0 ? console.log('Верно') : console.log('Не верно');
+
+// Task5
+// let a = 0;
+// a <= 0 ? console.log('Верно') : console.log('Не верно');
+
+// Task6
+// let a = 0;
+// a != 0 ? console.log('Верно') : console.log('Не верно');
+
+// Task7
+// let a = 0;
+// a === 'test' ? console.log('Верно') : console.log('Не верно');
+
+// Task8
+// let a = 0;
+// a === '1' ? console.log('Верно') : console.log('Не верно');
+
+// Task9
+/* let a = 2;
+if (a > 0 && a < 5){
+    console.log('Верно')
+} else {
+    console.log('Не верно')
+} */
+
+// Task10
+/* let a = 2;
+if (a === 0 || a === 2){
+    a += 7;
+} else {
+    a /= 10;
+} */
+
+//Task11
+/* let a = 2;
+let b = 4;
+if (a <= 1 && b >= 3){
+    const result = a + b;
+} else {
+    const result = a - b;
+} */
+
+//Task12
+/* let a = 3;
+let b = 5;
+if (a > 2 && a < 11 || b >= 6 && b < 14){
+    console.log('Верно')
+} else {
+    console.log('Не верно')
+} */
+
+//Task13
+let num = 3;
+let result;
+switch (num){
+    case 1:
+        result = 'winter';
+        break;
+    case 2:
+        result = 'spring';
+        break;
+    case 3:
+        result = 'summer';
+        break;
+    case 4:
+        result = 'fall';
+        break;
+    default:
+        result = 'Error! Incorrect number!';
+}
+
+console.log(result)
+
+
+
